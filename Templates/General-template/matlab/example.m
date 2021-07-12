22248facb948521c81ff097b3d2f7a2f6f93c37c
jjholt <jjholt@MPB>
2021-07-12 19:22:56 +0100

    Reintroduced template

--- Templates/General-template/matlab/example.m
+++
@@ -1,7 +0,0 @@
-x=linspace(0,2*pi);
-y=sin(x);
-plot(x,y);
-xlabel('x'); ylabel('f(x)'); axis square
-legend('f(x)=sin(x)');
-print('figures/graph.eps','-depsc')
-matlab2tikz('figures/graph.tex', 'height', '\fheight', 'width', '\fwidth');
